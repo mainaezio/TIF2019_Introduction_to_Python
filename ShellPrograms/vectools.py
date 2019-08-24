44a9108ed5ded00a4d5b8f7061611d7d53716c9c
Ezio Maina <ezio.maina@unito.it>
2019-08-24 18:17:31 +0200

    first commit

---
+++ ShellPrograms/vectools.py
@@ -0,0 +1,24 @@
+import math
+
+import numpy as np
+
+
+def norm(x):
+    """returns the magnitude of a vector x"""
+    return math.sqrt(sum(x ** 2))
+
+
+def unitvector(x):
+    """returns a unit vector x/|x|. x needs to be a numpy array."""
+    xnorm = norm(x)
+    if xnorm == 0:
+        raise ValueError("Can't normalise vector with length 0")
+    return x / norm(x)
+
+
+if __name__ == "__main__":
+    #a little demo of how the functions in this module can be used:
+    x1 = np.array([0, 1, 2])
+    print("The norm of " + str(x1) + " is " + str(norm(x1)) + ".")
+    print("The unitvector in direction of " + str(x1) + " is " \
+        + str(unitvector(x1)) + ".")
